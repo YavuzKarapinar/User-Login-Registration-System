7b53c37a559b150fcbb4edf2edd0ba222a010568
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-02-17 12:24:47 +0300

    init

---
+++ src/main/java/me/jazzy/userloginregistrationapp/UserLoginRegistrationAppApplication.java
@@ -0,0 +1,13 @@
+package me.jazzy.userloginregistrationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserLoginRegistrationAppApplication.class, args);
+    }
+
+}
