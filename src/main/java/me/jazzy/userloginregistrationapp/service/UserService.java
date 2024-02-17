9c33c2aa01b54cd0b7ddc47d61c5a7d70078b36b
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-02-17 13:19:57 +0300

    feat: password encoded

--- src/main/java/me/jazzy/userloginregistrationapp/service/UserService.java
+++ src/main/java/me/jazzy/userloginregistrationapp/service/UserService.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import me.jazzy.userloginregistrationapp.model.ConfirmationToken;
 import me.jazzy.userloginregistrationapp.model.User;
 import me.jazzy.userloginregistrationapp.repository.UserRepository;
+import me.jazzy.userloginregistrationapp.security.PasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,6 +19,7 @@ import java.util.UUID;
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final ConfirmationTokenService confirmationTokenService;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -32,6 +35,9 @@ public class UserService implements UserDetailsService {
 
         if(userExists)
             throw new IllegalStateException("Email already taken");
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
 
         userRepository.save(user);
 
