33cc0720fcb1e6b7c42b83c18dd4dfbd78b34b73
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-02-18 14:28:12 +0300

    change: Response Entity changed from ResponseEntity.ok() to new ResponseEntity<>()

--- src/main/java/me/jazzy/userloginregistrationapp/controller/RegisterController.java
+++ src/main/java/me/jazzy/userloginregistrationapp/controller/RegisterController.java
@@ -3,6 +3,7 @@ package me.jazzy.userloginregistrationapp.controller;
 import lombok.AllArgsConstructor;
 import me.jazzy.userloginregistrationapp.model.RegisterRequest;
 import me.jazzy.userloginregistrationapp.service.RegisterService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +16,12 @@ public class RegisterController {
 
     @PostMapping
     public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(registerService.register(request));
+        return new ResponseEntity<>(registerService.register(request), HttpStatus.OK);
     }
 
     @GetMapping("confirm")
     public ResponseEntity<String> confirm(@RequestParam String token) {
-        return ResponseEntity.ok(registerService.confirmToken(token));
+        return new ResponseEntity<>(registerService.confirmToken(token), HttpStatus.OK);
     }
 
 }
